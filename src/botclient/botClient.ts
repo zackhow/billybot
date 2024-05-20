9f9806a31a13ea05e562d32e1d33741f6a059fa6
zackhow <zackhow@gmail.com>
2024-05-19 20:00:25 -0400

    Cleanup ts

--- src/botclient/botClient.ts
+++ src/botclient/botClient.ts
@@ -1,15 +1,10 @@
 import {Client, Events, GatewayIntentBits, WebhookClient} from "discord.js";
-import {getRepository} from "../data-source.js";
-import {GuildEntity} from "../entity/GuildEntity.js";
-import {ActionEntry} from "../entity/ActionEntry.js";
 import {MODNOTES, MODNOTESCLEAR, PING} from "./commands.js";
 import {addModnotesListeners, disableModnotes, enableModnotes} from "./modnotes.js";
 
 const token = process.env.DISCORD_TOKEN;
-const webhook = process.env.WEBHOOK_URL;
 
 export const client = new Client({ intents: [GatewayIntentBits.GuildMembers, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
-const wc = new WebhookClient({url: webhook});
 
 client.once(Events.ClientReady, readyClient => {
     console.log(`Ready! Logged in as ${readyClient.user.tag}`);
